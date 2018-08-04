a272d4f0768f843703469f1c41c0a8fe5f2fa4fb
neaty.soopaul@gmail.com <neaty.soopaul@gmail.com>
2018-08-04 23:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ else if (string === string.toUpperCase()) {
 return "YES INDEED!"
 }
 else if (string === "I love you, Grandma.") {
-return (string.mixedCase "I love you too.")
+return ("I love you too.")
 }
 }
