4de69403ec17d02a3d425a40f8b9fdf5adece94b
neaty.soopaul@gmail.com <neaty.soopaul@gmail.com>
2018-08-04 21:35:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout (string) {
+  return string.toUpperCase()
+}
+
+function whisper (string) {
+  return string.toLowerCase()
+}
+
+function logShout (string) {
+  console.log.toUpperCase()
+}
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', 
+function require (spy) 
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
